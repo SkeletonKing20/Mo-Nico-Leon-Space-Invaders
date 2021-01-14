750e223f5595f098852ad24d83ce39d4a1744d64
SkeletonKing20 <n.bonn2002@gmail.com>
2021-01-14 18:11:58 +0100

    Shit was fucked yo

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -3,7 +3,21 @@
 public class Enemy : MonoBehaviour
 {
     private EnemySpawner spawner;
-
+    private float speed = 0.2f;
+    private float rightBorder = 8;
+    private float leftBorder = 8;
+    public void MoveEnemy()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+        foreach (Enemy enemy in spawner.GetSpawnerChildren())
+        {
+            if (transform.position.x < leftBorder || transform.position.x < rightBorder)
+            {
+                speed = -speed;
+                return;
+            }
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         OnEnemyHit();
